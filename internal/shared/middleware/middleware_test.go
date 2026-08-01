f3c9663768a9022f4cef3c830cd8f57eddd385c2
viethung213 <dangviethung213@gmail.com>
2026-08-01 09:39:58 +0700

    🎨 style(middleware): format middleware_test.go imports to match gci default config

--- internal/shared/middleware/middleware_test.go
+++ internal/shared/middleware/middleware_test.go
@@ -13,13 +13,12 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	_ "github.com/viethung213/gym-companion/internal/gen/go/contracts/generic/auth/v1/service"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
-
-	_ "github.com/viethung213/gym-companion/internal/gen/go/contracts/generic/auth/v1/service"
 )
 
 var (
